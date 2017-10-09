b6b362ceb548dd6ac90cf5128df1fcf87ed34094
Anthony Limere <anthonylimere@gmail.com>
2017-10-09 11:33:08 +0200

    feat: Add server-side GoogleOauth

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: 'mongodb://antholimere:password@ds113435.mlab.com:13435/fycto-dev',
+  googleClientID: '176283088354-qjflvl5sojp0cq3lj82lkrhp6t0ghkjc.apps.googleusercontent.com',
+  googleClientSecret: 'zp1iaRgyodwakw5PS40u9h3Z',
+  cookieKey: 'aoisfjasojfaosifjaosifjaosjfaisojf'
+};
