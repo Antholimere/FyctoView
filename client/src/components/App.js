22286bcb6af628cc1c223889fb0bd583243d4753
Anthony Limere <anthonylimere@gmail.com>
2017-10-09 12:52:54 +0200

    feat: Add header component and material-ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
+import './App.css';
 import Header from './Header';
+
 const Landing   = () => <h2>Landing</h2>
 
 class App extends Component {
@@ -13,7 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div>
         <BrowserRouter>
           <div>
             <Header />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Header = () => {
-  return(
-    <div>Header</div>
-  )
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+const styles = {
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  },
+  appBar: {
+    background: '#1D8BF1'
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+    textDecoration: 'none',
+    color: 'inherit'
+  }
+};
+
+class Header extends Component {
+  renderContent() {
+    console.log(this.props.auth)
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <p><a style={styles.menuButton} href="/auth/google">Login with Google</a></p>;
+      default:
+        return <p><a style={styles.menuButton} href="/api/logout">Logout</a></p>;
+    }
+  }
+
+  render(){
+    return (
+      <div style={styles.root}>
+        <AppBar style={styles.appBar}>
+          <Toolbar>
+            <Typography type="title" color="inherit" style={styles.flex}>
+              FV
+            </Typography>
+            {this.renderContent()}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
+
+
+
