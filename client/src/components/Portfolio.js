dc3f69c1744f1f5d756834dfc12d36a470fb1689
Anthony Limere <anthonylimere@gmail.com>
2017-10-17 17:15:56 +0200

    feat: Add StatsBar component to portfolio

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import InvestmentList from './investments/InvestmentList';
+import InvestmentStatsBar from './investments/InvestmentStatsBar';
 
 const Portfolio = () => {
   return(
     <div>
+      <InvestmentStatsBar />
       <h4>Portfolio</h4>
       <InvestmentList />
     </div>
---
+++ client/src/components/investments/InvestmentStatsBar.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import Typography from 'material-ui/Typography';
+import { connect } from 'react-redux';
+import moment from 'moment'
+
+class InvestmentStatsBar extends Component {
+
+  calculateTotalInvestedDollars (investments) {
+    let total = 0
+    investments.map(i => {
+      total += (i.units * i.dollarValue)
+    })
+    return total
+  }
+
+  kFormatter(num) {
+    return num > 999 ? (num/1000).toFixed(1) + 'k' : num
+  }
+
+  getCurrentPrice(currency) {
+    const price = this.props.crypto.filter((c) => {
+      if (c.id === currency) {
+        return(
+          c
+        )
+      }
+    })
+    return price[0].price_usd
+  }
+
+  calculateTotalInvestedTime (investments) {
+    const earliest = new Date(Math.min.apply(null, investments.map(function(e) {
+      return new Date(e.date);
+    })));
+
+    return moment(earliest).fromNow(true)
+  }
+
+  calculateTotalProfitPercent (investments) {
+    let total = 0;
+
+    investments.map(i => {
+      const currentPriceOne = parseInt(this.getCurrentPrice(i.currency), 10)
+      const currentPriceUnits = currentPriceOne * i.units
+      const pastPriceUnits = i.units * parseInt(i.dollarValue, 10)
+      const percentProfit = parseInt((currentPriceUnits / pastPriceUnits) * 100, 10) - 100
+      total += percentProfit;
+    })
+    return total;
+  }
+
+  calculateTotalBalance (investments) {
+    let total = 0;
+
+    investments.map(i => {
+      const currentPriceOne = parseInt(this.getCurrentPrice(i.currency), 10)
+      const currentPriceUnits = currentPriceOne * i.units
+      total += currentPriceUnits;
+    })
+    return total;
+  }
+
+  renderContent() {
+    return(
+      <Typography type="subheading" align="center" gutterBottom>
+        <span> Invested {this.kFormatter(this.calculateTotalInvestedDollars(this.props.investments))}$ </span>
+        |
+        <span> Turnover +{this.calculateTotalProfitPercent(this.props.investments)}% </span>
+        |
+        <span> Total balance {this.kFormatter(this.calculateTotalBalance(this.props.investments))}$ </span>
+        |
+        <span> in {this.calculateTotalInvestedTime(this.props.investments)}</span>
+      </Typography>
+    )
+  }
+
+  render() {
+    return(
+      <div>
+        {(this.props.investments && this.props.crypto) && this.renderContent()}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ investments, crypto }) {
+  return { investments, crypto }
+}
+
+export default connect(mapStateToProps)(InvestmentStatsBar);
