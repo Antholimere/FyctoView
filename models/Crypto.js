a98bf16cf64215dad68470cfcb6c1be1badefd46
Anthony Limere <anthonylimere@gmail.com>
2017-10-19 09:38:23 +0200

    refactor: Remove unused models and service

--- models/Crypto.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const cryptoSchema = new Schema({
-  name: String,
-  symbol: String
-});
-
-mongoose.model('cryptos', cryptoSchema)
